78a64b2b2f6aaca756a9cc868c85fde9f537a88d
mythicoco <talavy201@email.com>
2025-08-19 07:22:41 +0300

    please work dammn

--- src/handleSignInGoogle.tsx
+++ src/handleSignInGoogle.tsx
@@ -4,7 +4,7 @@ async function handleSignInGoogle() {
     await Supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `${location.origin}/auth/callback`,
+      redirectTo: `${location.origin}/Mythic/auth/callback`,
       queryParams: {
         access_type: 'offline',
         prompt: 'consent'
